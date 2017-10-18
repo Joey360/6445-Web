d6bfcf1cbef6681b7eba40a7306880c431452a5c
Joey360 <joey360@comcast.net>
2017-10-17 21:01:29 -0700

    Add files, UNSECURE ¯\_(ツ)_/¯

---
+++ cgi-bin/main.py
@@ -0,0 +1,47 @@
+from pymongo import MongoClient
+import cgi
+import bcrypt
+
+forms = cgi.FieldStorage
+import flask
+from datetime import datetime
+import json
+from collections import namedtuple
+
+client = MongoClient(
+    "mongodb://CTECROBOTICS:Qzmtl7Xnr4OW1kwN@main-shard-00-00-h24xb.mongodb.net:27017,main-shard-00-01-h24xb.mongodb.net:27017,main-shard-00-02-h24xb.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin")
+db = client['ScouterData']
+Login = client['Login']
+LOGS = client['Logs']
+TeamStats = db['Team-Stats']
+username_c = forms["username"].value
+password = forms["password"].value
+
+if username_c == Login.findone({"username": username_c}):
+    all_data = Login.findone({"username": username_c})
+    x = json.loads(all_data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+    print(x.username, x.password)
+
+gear = None
+gearComments = None
+teamComments = None
+canClimb = None
+attemptedClimb = None
+ballsCollected = None
+avoidedBalls = None
+teamName = None
+teamNumber = None
+ctime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+stats = {
+    "Gears": gear,
+    "GearComments": gearComments,
+    "Climb": canClimb,
+    "AttemptedClimb": attemptedClimb,
+    "BallsCollected": ballsCollected,
+    "AvoidedBalls": avoidedBalls,
+    "TeamName": teamName,
+    "TeamNumber": teamNumber,
+    "TeamComments": teamComments,
+    "Time&Date": ctime,
+}
+TeamStats.insert(stats)
---
+++ cgi-bin/username_pass_creator.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+import bcrypt
+import json
+from collections import namedtuple
+
+client = MongoClient(
+    "mongodb://CTECROBOTICS:Qzmtl7Xnr4OW1kwN@main-shard-00-00-h24xb.mongodb.net:27017,main-shard-00-01-h24xb.mongodb.net:27017,main-shard-00-02-h24xb.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin")
+db = client['ScouterData']
+Login = client['Login']
+username = input("username")
+result = db.Login.find({"username": username})
+if result.count() > 0:
+    print("you have already created an account")
+    exit()
+
+password = input("password")
+salt = bcrypt.gensalt()
+hashed_pw = bcrypt.hashpw(password.encode('utf8'), salt)
+print(hashed_pw)
+print(salt)
+account = {
+    "username": username,
+    "password": hashed_pw,
+    "salt": salt,
+}
+db.Login.insert(account)
