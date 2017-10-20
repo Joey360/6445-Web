6b0108b7d74457d71543fef7fdc015f533995da4
Joey360 <joey360@comcast.net>
2017-10-19 18:14:26 -0700

    changes

--- join/join.php
+++ join/join.php
@@ -10,6 +10,7 @@
     <!-- Main CSS -->
 		<link rel="stylesheet" type="text/css" href="/includes/main.css"/>
   </head>
+  <?php $Name = "Name"; ?>
   <body>
     <img  src="images/logo.jpg" alt="blank" class="img-responsive">
     <div id="Form-Join">
@@ -25,31 +26,6 @@
       <input type="checkbox" name="interest" value="Business" id="cb-Support">
       <input type="checkbox" name="interest" value="Photography/Video" id="cb-Photography/Media">
       <input type="checkbox" name="unknown interest" value="0" id="cb-Undecided">
-      <script type="text/javascript">
-        if(){
-          var programming = document.getElementById("cb-Programming").checked;
-          var mechanical = document.getElementById("cb-Mechanical").checked;
-          var cad = document.getElementById("cb-CAD").checked;
-          var support = document.getElementById("cb-Support").checked;
-          var photography = document.getElementById("cb-Photography/Media").checked;
-          var undecided = document.getElementById("cb-Undecided").checked;
-          var any_true;
-          if (programming == True) {
-             any_true = True
-          }
-          else if (mechanical == True) {
-            any_true
-          } else if (cad == True) {
-            any_true = True
-          } else if (support == True) {
-
-          }else if (photography == True) {
-
-          } else if () {
-
-          }
-        }
-      </script>
       <input type="checkbox" name="Grade-9" value="9">
       <input type="checkbox" name="Grade-10" value="10">
       <input type="checkbox" name="Grade-11" value="11">
