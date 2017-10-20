208c21ad06c6f3ea576bf0493147f4ffc6b96dd8
Samdlee <samlee2001@outlook.com>
2017-10-20 16:12:23 -0700

    changes

---
+++ home/home.php
@@ -0,0 +1,56 @@
+
+
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width = device-width, initial-scale=1">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Main CSS -->
+		<link rel="stylesheet" type="text/css" href="/includes/main.css"/>
+  </head>
+  <body>
+    <div class="container row-spacing">
+ 							  <div id="myCarousel" class="carousel slide col-xs-10 col-xs-offset-1 " data-ride="carousel">
+
+							    <!-- Indicators -->
+							    <ol class="carousel-indicators">
+							      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+							      <li data-target="#myCarousel" data-slide-to="1"></li>
+							      <li data-target="#myCarousel" data-slide-to="2"></li>
+							    </ol>
+
+							    <!-- Wrapper for slides -->
+							    <div class="carousel-inner">
+							      <div class="item active">
+							        <img src="includes/images/img1.jpg" alt="_Image _Blank_" style="height: 500px;" class=" center-block">
+							      </div>
+
+							      <div class="item">
+							        <img src="includes/images/img2.jpg" alt="_Image _Blank_" style="height: 500px;" class=" center-block">
+							      </div>
+
+							      <div class="item">
+							        <img src="includes/images/img3.jpg" alt="_Image _Blank_" style="height: 500px;" class=" center-block">
+							      </div>
+							    </div>
+
+							    <!-- Left and right controls -->
+							    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+							      <span class="glyphicon glyphicon-chevron-left"></span>
+							      <span class="sr-only">Previous</span>
+							    </a>
+							    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+							      <span class="glyphicon glyphicon-chevron-right"></span>
+							      <span class="sr-only">Next</span>
+							    </a>
+							  </div>
+								<div class="row row-spacing">
+                  <div class="col-xs-12">
+                    <?php include ("/calander/calander.html");?>
+                  </div>
+                </div>
+      </div>
