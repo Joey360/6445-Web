164199cda6b78c0d1bbad851100d3d685af51507
Samdlee <samlee2001@outlook.com>
2017-10-23 15:19:02 -0700

    cc

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" type="text/css" href="home/home.css"/>
     <link rel="stylesheet" type="text/css" href="team-scouting/team-scouting.css"/>
 
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 
 	<body><div class="container-fluid">
