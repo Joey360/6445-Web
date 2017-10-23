bd0654eb6dcbd64070e5f9a93861afd2dfcafcfd
Samdlee <samlee2001@outlook.com>
2017-10-23 15:35:30 -0700

    cc

--- join/join.php
+++ join/join.php
@@ -20,7 +20,7 @@
         $Message = "Hello, " . $Name . " Is interested in joining the team!" . " You can contact them at " . $PhoneNumber . " and at " . $Email;
         $Message .= ", they are interested in " . $interest . " and is in the " . $Grade . " Grade.";
       //  mail($to, $subject, $Message, $headers);
-        echo("This is what the message would look like if the mail server was setup: " . $Message);
+        // echo("This is what the message would look like if the mail server was setup: " . $Message);
     }
     ?>
 <div class="container">
