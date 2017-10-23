ccc97d9618f9f18e3fcc9d3139cd0a08a74e50b3
Samdlee <samlee2001@outlook.com>
2017-10-23 16:21:59 -0700

    cc

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" type="text/css" href="home/home.css"/>
     <link rel="stylesheet" type="text/css" href="team-scouting/team-scouting.css"/>
 
+		<link rel="stylesheet" type="text/css" href="about/instructors/instructors.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 
